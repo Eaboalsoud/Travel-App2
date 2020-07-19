3bd098b8782dfc5155b2e76e242393f874670ea2
Eaboalsoud <63580970+Eaboalsoud@users.noreply.github.com>
2020-07-19 23:31:08 +0300

    Added Git ignore file

---
+++ test/test/testapp.test.js
@@ -0,0 +1,14 @@
+import {performAction } from "../src/client/js/app"
+
+
+
+describe("Testing the hperformAction functionality", () => {
+    
+    test("Testing the performAction() function", () => {
+           
+           expect(performAction).toBeDefined();
+})});
+
+
+
+
